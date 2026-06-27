27ef638c4d821722c9e42c148cd4be4e7801b841
borkeled <borschlan@gmail.com>
2026-06-27 15:01:58 -0700

    fix: Adjust vertical scroll maximum calculation to account for extra bottom clearance in child windows

--- Bitware/Source/Core/UI/GOOD/helpers/draw.cpp
+++ Bitware/Source/Core/UI/GOOD/helpers/draw.cpp
@@ -177,7 +177,8 @@ void c_draw::text(ImDrawList* draw_list, const ImFont* font, float font_size, co
         clip_rect.z = ImMin(clip_rect.z, cpu_fine_clip_rect->z);
         clip_rect.w = ImMin(clip_rect.w, cpu_fine_clip_rect->w);
     }
-    font->RenderText(draw_list, font_size, pos, col, clip_rect, text_begin, text_end, wrap_width, cpu_fine_clip_rect != NULL);
+    ImVec2 rounded_pos(roundf(pos.x), roundf(pos.y));
+    font->RenderText(draw_list, font_size, rounded_pos, col, clip_rect, text_begin, text_end, wrap_width, cpu_fine_clip_rect != NULL);
 }
 
 void c_draw::text_clipped(ImDrawList* draw_list, ImFont* font, const ImVec2& pos_min, const ImVec2& pos_max, ImU32 color, const char* text, const char* text_display_end, const ImVec2* text_size_if_known, const ImVec2& align, const ImRect* clip_rect)
